02502b7e985c69a7dc6a9603ebe9dcfee37e508c
ivanmlakic <ivan.mlakic@boosterfuels.com>
2023-06-13 22:27:12 +0200

    input message handler and useState (for newMessage) added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 import SendMessage from './components/SendMessage'
 import ViewMessages from './components/ViewMessages'
 
 function App() {
+    const [newMessage, setNewMessage] = useState(null)
+
     return (
         <>
             <div>
                 <ViewMessages />
-                <SendMessage />
+                <SendMessage setNewMessage={setNewMessage} />
+                {console.log(
+                    'App rendered, newMessage: ' + JSON.stringify(newMessage)
+                )}
             </div>
         </>
     )
--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const SendMessage = () => {
-    return <div>Send Message</div>
+const SendMessage = ({ setNewMessage }) => {
+    const newMessageHandler = (event) => {
+        setNewMessage({ text: event.target.value })
+    }
+    return (
+        <>
+            <input type="text" onChange={newMessageHandler}></input>
+            <button>Send</button>
+        </>
+    )
 }
 
 export default SendMessage
